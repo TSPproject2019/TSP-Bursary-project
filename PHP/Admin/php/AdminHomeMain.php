fc43ac0e84bbd5f587511572b6bc807646dd6063
Mike Wright <mike.wright@mikat.co.uk>
2019-02-10 16:02:31 +0000

    updates made to use php

---
+++ PHP/Admin/php/AdminHomeMain.php
@@ -0,0 +1,80 @@
+ 
+        <div class="row">
+            <div class="dropdown col-3 ml-4">
+                  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     <i class="fas fa-list-ul"></i></a>
+                       
+                      <div class="dropdown-menu bg-secondary" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="Review Student Submissions.html">Review Student Submissions</a>
+                        <a class="dropdown-item" href="Review Staff Submissions.html">Review Staff Submissions</a>
+                        <a class="dropdown-item" href="History Student Requests.html">History Student Requests</a>
+                        <a class="dropdown-item" href="History Staff Requests.html">History Staff Requests</a>
+                        <a class="dropdown-item" href="FAQ.html">FAQ</a>
+                        <a class="dropdown-item" href="Agreement Form.html">Agreement Form</a>
+                    </div>
+               </div>
+                
+                <div class="col-3">
+                    <ul class="list-group">
+                       <li class="list-group-item  border-0">Submitted: <span>10</span></li>
+                        <li class="list-group-item  border-0">Approved: <span>8</span></li>
+                        <li class="list-group-item  border-0">Awaiting delivery: <span>YES</span></li>
+                    </ul>
+                </div>
+          </div>
+          
+ <section class="container">         
+            <h1 class="text-center">Admin Home</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et voluptate nam, aliquid amet maiores fuga,
+                    facilis ipsam soluta,
+                    reprehenderit, explicabo repellat. Maiores libero mollitia esse illo. Nam, officia, quisquam!
+                </p>
+   </section>     
+         
+    
---
+++ PHP/Shared/php/AllHeader.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Admin Home Page </title>
+
+ <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="../CSS/styles.css">
+
+    <!-- Bootstrap 4.1 CND -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Font Awsome icons -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+   
+   <!--Include page name in header -->
+  
+    
+</head>
+
+<body>
+    <!-- <?php include('header.php'); ?> -->
+    
+ <header class="jumbotron">
+    <div class="row text-center">
+        <div class="col-lg-6">
+                <h2>Welcome (Name)</h2>
+                <p>Course details</p>
+        </div>
+        
+        <div class="col-lg-6">
+            <img src="../Staff/Images/logo1.png" class="img-fluid" width="100px", height="50px" alt="Responsive image"></img>
+            <a href="" class="btn btn-outline-success btn-sm">Log out</a>
+        </div>
+    </div>
+ </header>
