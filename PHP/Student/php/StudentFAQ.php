8f345153ee88fd87b55bb66d307946f1e59badeb
Mike Wright <mike.wright@mikat.co.uk>
2019-02-17 18:39:07 +0000

    student *.php load scripts now created, jsut needs balance added.

--- PHP/Student/php/StudentFAQ.php
+++ PHP/Student/php/StudentFAQ.php
@@ -1,5 +1,71 @@
 <?php
     session_start();
+   # echo " start Step 0.0..<br>"; // for testing purposes
+    require_once 'connect.php';//connects to the SQL database.
+   # echo " start Step 1.0..<br>"; // for testing purposes
+    // functions
+    
+    function getTotals ($uID, $stat){
+        global $totalResult;
+        require 'connect.php';//connects to the SQL database.
+       # echo " start Step 4.0..<br>"; // for testing purposes
+        $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
+          INNER JOIN itemsAndRequests WHERE itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
+          AND itemsAndRequests.StudentID = " . $uID . " AND bursaryRequests.bRequestsStaffApproved is NULL 
+          AND bursaryRequests.bRequestsAdminApproved is NULL 
+          AND bRequestsStatus = '" . $stat . "'";
+        $totalResult = 0;
+        // now to run the query
+        # echo " start Step 4.1..<br>"; // for testing purposes
+        // first prepare and excecurte
+        $result = $DBconnection->query($SQL_stmt);
+        # echo " start Step 4.2..<br>"; // for testing purposes
+        // now get the data
+        if ($row = $result->fetch()){
+            // varify that it is a valid userID
+            # echo " start Step 4.2.1..<br>"; // for testing purposes
+            // Bind results by column name
+            $totalResult = $row['Total'];
+            #return $submitTotal;
+        }
+        return $totalResult;
+    }
+    // end functions
+    
+    // Get the _SESSION user details.
+    if (isset($_SESSION['lastName'])){
+       # echo " start Step 2.0..<br>"; // for testing purposes
+        $firstName = $_SESSION['firstName'];
+        $lastName = $_SESSION['lastName'];
+        $userid = $_SESSION['userid'];
+        $userName = $firstName . " " . $lastName;
+        // get course title
+        $SQL_stmt = "SELECT DISTINCT courseTitle FROM course 
+        INNER JOIN studentToCourse ON course.courseID = studentToCourse.stcCourseID
+        INNER JOIN users ON users.userID = " . $userid . " and studentToCourse.stcStudentID = '" . $userid . "'";
+        // now to run the query
+
+        //
+       # echo " start Step 2.0..<br>"; // for testing purposes
+        // first prepare and excecurte
+        $result = $DBconnection->query($SQL_stmt);
+       # echo " start Step 2.1..<br>"; // for testing purposes
+        // now get the data
+        if ($row = $result->fetch()){
+            // varify that it is a valid userID
+           # echo " start Step 2.1.1..<br>"; // for testing purposes
+            // Bind results by column name
+            $courseTitle = $row['courseTitle'];
+            // store session variables
+            $_SESSION['courseTitle'] =  $courseTitle;
+            // this varisable is also used for posting.
+
+        }
+        // get the data for the submitted requests
+        $submitTotal = getTotals ($userid, "Submitted");
+        $approved = getTotals ($userid, "Approved");
+        $pending = getTotals ($userid, "Pending");
+    }
 ?>
         <!-- <div class="row col-lg-6 justify-content-start align-items-center"> -->
         <div>
@@ -10,9 +76,11 @@
                 </div>
                 <div class="col-md-4 ml-4">
                     <ul class="list-group list-group-flush">
-                       <li class="list-group-item">Submitted:</li>
-                        <li class="list-group-item">Approved:</li>
-                        <li class="list-group-item">Awaiting delivery:</li>
+                      <?php
+                        echo '<li class="list-group-item">Submitted: <span>' . $submitTotal . '</span></li>';
+                        echo '<li class="list-group-item">Approved: <span>' . $approved . '</span></li>';
+                        echo '<li class="list-group-item">Awaiting delivery: <span>' . $pending . '</span></li>';
+                      ?>
                     </ul>
                 </div>
           </div>
--- PHP/Student/php/StudentNewRequest.php
+++ PHP/Student/php/StudentNewRequest.php
@@ -6,7 +6,7 @@
     function getTotals ($uID, $stat){
         global $totalResult;
         require 'connect.php';//connects to the SQL database.
-      #  echo " start Step 4.0..<br>"; // for testing purposes
+       # echo " start Step 4.0..<br>"; // for testing purposes
         $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
           INNER JOIN itemsAndRequests WHERE itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
           AND itemsAndRequests.StudentID = " . $uID . " AND bursaryRequests.bRequestsStaffApproved is NULL 
@@ -17,11 +17,11 @@
        #  echo " start Step 4.1..<br>"; // for testing purposes
         // first prepare and excecurte
         $result = $DBconnection->query($SQL_stmt);
-        # echo " start Step 4.2..<br>"; // for testing purposes
+       #  echo " start Step 4.2..<br>"; // for testing purposes
         // now get the data
         if ($row = $result->fetch()){
             // varify that it is a valid userID
-            # echo " start Step 4.2.1..<br>"; // for testing purposes
+           #  echo " start Step 4.2.1..<br>"; // for testing purposes
             // Bind results by column name
             $totalResult = $row['Total'];
             #return $submitTotal;
