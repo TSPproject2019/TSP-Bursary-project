ab7ab7fa6e8b87a90b858ba3a4cbcd60c7df14b6
Mike Wright <mike.wright@mikat.co.uk>
2019-02-17 17:58:34 +0000

    load set php sctipts for StudentNewRequest.php

--- PHP/Student/php/StudentNewRequest.php
+++ PHP/Student/php/StudentNewRequest.php
@@ -1,9 +1,39 @@
 <?php
     session_start();
     require_once 'connect.php';//connects to the SQL database.
+    // functions
+    //
+    function getTotals ($uID, $stat){
+        global $totalResult;
+        require 'connect.php';//connects to the SQL database.
+      #  echo " start Step 4.0..<br>"; // for testing purposes
+        $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
+          INNER JOIN itemsAndRequests WHERE itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
+          AND itemsAndRequests.StudentID = " . $uID . " AND bursaryRequests.bRequestsStaffApproved is NULL 
+          AND bursaryRequests.bRequestsAdminApproved is NULL 
+          AND bRequestsStatus = '" . $stat . "'";
+        $totalResult = 0;
+        // now to run the query
+       #  echo " start Step 4.1..<br>"; // for testing purposes
+        // first prepare and excecurte
+        $result = $DBconnection->query($SQL_stmt);
+        # echo " start Step 4.2..<br>"; // for testing purposes
+        // now get the data
+        if ($row = $result->fetch()){
+            // varify that it is a valid userID
+            # echo " start Step 4.2.1..<br>"; // for testing purposes
+            // Bind results by column name
+            $totalResult = $row['Total'];
+            #return $submitTotal;
+        }
+        return $totalResult;
+    }
+
+
+    // end functions
     // Get the _SESSION user details.
     if (isset($_SESSION['lastName'])){
-        echo " start Step 2.0..<br>"; // for testing purposes
+       # echo " start Step 2.0..<br>"; // for testing purposes
         $firstName = $_SESSION['firstName'];
         $lastName = $_SESSION['lastName'];
         $userid = $_SESSION['userid'];
@@ -15,14 +45,14 @@
         // now to run the query
 
         //
-        echo " start Step 2.0..<br>"; // for testing purposes
+       # echo " start Step 2.0..<br>"; // for testing purposes
         // first prepare and excecurte
         $result = $DBconnection->query($SQL_stmt);
-        echo " start Step 2.1..<br>"; // for testing purposes
+       # echo " start Step 2.1..<br>"; // for testing purposes
         // now get the data
         if ($row = $result->fetch()){
             // varify that it is a valid userID
-            echo " start Step 2.1.1..<br>"; // for testing purposes
+           # echo " start Step 2.1.1..<br>"; // for testing purposes
             // Bind results by column name
             $courseTitle = $row['courseTitle'];
             // store session variables
@@ -35,14 +65,14 @@
         INNER JOIN departmentsStaffCourseStudents ON users.userID = departmentsStaffCourseStudents.bscsStaffID
         AND departmentsStaffCourseStudents.bscsStudentID  = '" . $userid . "'";
         // now to run the query
-        echo " start Step 3.0..<br>"; // for testing purposes
+       # echo " start Step 3.0..<br>"; // for testing purposes
         // first prepare and excecurte
         $result = $DBconnection->query($SQL_stmt);
-        echo " start Step 3.1..<br>"; // for testing purposes
+       # echo " start Step 3.1..<br>"; // for testing purposes
         // now get the data
         if ($row = $result->fetch()){
             // varify that it is a valid userID
-            echo " start Step 3.1.1..<br>"; // for testing purposes
+           # echo " start Step 3.1.1..<br>"; // for testing purposes
             // Bind results by column name
             $courseTutorFirstName = $row['userFirstName'];
             $courseTutorLastName = $row['userLastName'];
@@ -53,25 +83,11 @@
             $_SESSION['courseTutorId'] =  $courseTutorId;
             // this varisable is also used for posting.
         }
+
         // get the data for the submitted requests
-        echo " start Step 4.0..<br>"; // for testing purposes
-        $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
-        INNER JOIN itemsAndRequests WHERE itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
-        AND itemsAndRequests.StudentID = " . $userid . " AND bursaryRequests.bRequestsStaffApproved is NULL 
-        AND bursaryRequests.bRequestsAdminApproved is NULL 
-        AND bRequestsStatus = 'Submitted'";
-        // now to run the query
-        echo " start Step 4.1..<br>"; // for testing purposes
-        // first prepare and excecurte
-        $result = $DBconnection->query($SQL_stmt);
-        echo " start Step 4.2..<br>"; // for testing purposes
-        // now get the data
-        if ($row = $result->fetch()){
-            // varify that it is a valid userID
-            echo " start Step 4.2.1..<br>"; // for testing purposes
-            // Bind results by column name
-            $submitTotal = $row['Total'];
-        }
+        $submitTotal = getTotals ($userid, "Submitted");
+        $approved = getTotals ($userid, "Approved");
+        $pending = getTotals ($userid, "Pending");
     }
 
 ?>
@@ -83,8 +99,8 @@
                     <ul class="list-group">
                         <?php
                             echo '<li class="list-group-item  border-0">Submitted: <span>' . $submitTotal . '</span></li>';
-                            echo '<li class="list-group-item  border-0">Approved: <span>8</span></li>';
-                            echo '<li class="list-group-item  border-0">Awaiting delivery: <span>YES</span></li>';
+                            echo '<li class="list-group-item  border-0">Approved: <span>' . $approved . '</span></li>';
+                            echo '<li class="list-group-item  border-0">Awaiting delivery: <span>' . $pending . '</span></li>';
                         ?>
                     </ul>
                 </div>
