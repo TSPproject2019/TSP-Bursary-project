6dbe3e3ef95a82440c1f4e066237ea7dc61c622e
Mike Wright <mike.wright@mikat.co.uk>
2019-02-18 09:22:17 +0000

    created functions.php, moved pointed to functions for students.*

--- PHP/Student/php/StudentFAQ.php
+++ PHP/Student/php/StudentFAQ.php
@@ -3,33 +3,7 @@
    # echo " start Step 0.0..<br>"; // for testing purposes
     require_once 'connect.php';//connects to the SQL database.
    # echo " start Step 1.0..<br>"; // for testing purposes
-    // functions
-    
-    function getTotals ($uID, $stat){
-        global $totalResult;
-        require 'connect.php';//connects to the SQL database.
-       # echo " start Step 4.0..<br>"; // for testing purposes
-        $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
-          INNER JOIN itemsAndRequests WHERE itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
-          AND itemsAndRequests.StudentID = " . $uID . " AND bursaryRequests.bRequestsStaffApproved is NULL 
-          AND bursaryRequests.bRequestsAdminApproved is NULL 
-          AND bRequestsStatus = '" . $stat . "'";
-        $totalResult = 0;
-        // now to run the query
-        # echo " start Step 4.1..<br>"; // for testing purposes
-        // first prepare and excecurte
-        $result = $DBconnection->query($SQL_stmt);
-        # echo " start Step 4.2..<br>"; // for testing purposes
-        // now get the data
-        if ($row = $result->fetch()){
-            // varify that it is a valid userID
-            # echo " start Step 4.2.1..<br>"; // for testing purposes
-            // Bind results by column name
-            $totalResult = $row['Total'];
-            #return $submitTotal;
-        }
-        return $totalResult;
-    }
+   require 'functions.php'; // connects to the functions.
     // end functions
     
     // Get the _SESSION user details.
