57785ee235a532b5e64b5a965205de5a2126ccdf
Mike Wright <mike.wright@mikat.co.uk>
2019-02-20 13:37:34 +0000

    updated small change, big one to follow

--- PHP/functions.php
+++ PHP/functions.php
@@ -28,4 +28,36 @@
         }
         return $totalResult;
     }
+
+    
+    // just workibng through query, and then will re-merge it if needed..
+    function getStaffTotals ($uID, $usrType, $stat){
+        global $staffTotalResult;
+        require 'connect.php';//connects to the SQL database.
+        // what is the required query data ?
+         if($usrType = 'Staff'){
+            $SQL_stmt = "SELECT COUNT(*) AS 'Total' FROM bursaryRequests
+            INNER JOIN itemsAndRequests ON itemsAndRequests.RequestID = bursaryRequests.bRequestsID 
+            AND bursaryRequests.bRequestsStaffID = " . $uID . " AND bursaryRequests.bRequestsStaffRequest = 'TRUE' 
+            AND bursaryRequests.bRequestsStaffApprove = 'Yes'
+            AND itemsAndRequests.StaffItemApproved = 'Yes'
+            AND bRequestsStatus = '" . $stat . "'";
+            $stafftotalResult = 0; // just incase this variable is holding any data, but should not be the case
+            // now to run the query
+            # echo " start Step 4.1..<br>"; // for testing purposes
+            // first prepare and excecurte
+            $result = $DBconnection->query($SQL_stmt);
+            # echo " start Step 4.2..<br>"; // for testing purposes
+            // now get the data
+            if ($row = $result->fetch()){
+                // varify that it is a valid userID
+                # echo " start Step 4.2.1..<br>"; // for testing purposes
+                // Bind results by column name
+                $stafftotalResult = $row['Total'];
+                #return $submitTotal;
+            }
+        }
+        return $staffTotalResult;
+    }
+   
 ?>
