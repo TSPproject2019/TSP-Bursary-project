4390aca12bd3069167c5f9a28e67753ecdbd7f6c
Mike Wright <mike.wright@mikat.co.uk>
2019-02-27 11:25:47 +0000

    workingthrough register

---
+++ Scripts/regScript.js
@@ -0,0 +1,21 @@
+  var pass = document.getElementById('password');
+  var popBox = document.getElementById('pPop');
+  var sub = document.getElementById('but1');
+  
+  function pop(){
+    popBox.style.display = "block";
+  }
+  
+  function popDown(){
+    popBox.style.display = "none";
+  }
+  
+  function butHov(){
+    sub.style.backgroundColor = black;
+    sub.style.borderColor = black;
+  }
+  
+  function butLeave(){
+    sub.style.backgroundColor = "#20134c";
+    sub.style.borderColor = "#20134c";
+  }
